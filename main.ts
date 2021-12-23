d8c9d2f6b66828461f33ea92d5c71f2b2f9bfafb
nohjungbin <85227130+nohjungbin@users.noreply.github.com>
2021-12-23 10:38:51 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,12 +1,36 @@
-input.onButtonPressed(Button.A, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.A, function () {
+    A_count += 1
 })
 input.onGesture(Gesture.SixG, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-    music.playTone(330, music.beat(BeatFraction.Whole))
+    B_count += 1
 })
+let A_count = 0
+let B_count = 0
 basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        if (true) {
+        	
+        } else {
+            if (true) {
+            	
+            } else {
+                if (true) {
+                	
+                } else {
+                    if (true) {
+                    	
+                    } else {
+                    	
+                    }
+                }
+            }
+        }
+    }
     music.setBuiltInSpeakerEnabled(true)
 })
